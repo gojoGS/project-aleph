789c3d3d0157a83405d5f58861f356438295a48e
Bartha Zoltán <barthazoli00@gmail.com>
2022-07-24 17:51:54 +0200

    removed frontend

--- frontend/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import preprocess from "svelte/types/compiler/preprocess";
-
-export default {
-    plugins: [svelte({ preprocess: preprocess() })],
-};
--- frontend/src/routes/routes.ts
+++
@@ -1,4 +0,0 @@
-export const ROUTE_ROOT = "/";
-export const ROUTE_MODULES = ROUTE_ROOT + "modules";
-export const ROUTE_ABOUT = ROUTE_ROOT + "about";
-export const ROUTE_CONTACT = ROUTE_ROOT + "contact";
